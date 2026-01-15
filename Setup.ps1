81ff02d8f0e2fc32c5ba15b09998d170a423d340
MINEFLFQ <wy20060614@outlook.com>
2026-01-15 19:53:52 +0800

    Initial commit

---
+++ Setup.ps1
@@ -0,0 +1,7 @@
+# 在当前文件夹中执行（PowerShell）
+git init
+git add --all
+git commit -m "Initial commit"
+git branch -M main
+git remote add origin https://github.com/MINEFLFQ/Geminorum-s-PlayerLog.git
+git push -u origin main --force
